3c53fb0008a1dbadf6ca87f37eb370c2a9a57b9e
IsraelSerrano1 <israel.serrano@softtek.com>
2023-05-24 13:27:48 +0200

    Resolucion Ejercicio Terminado

--- src/main/java/com/softtek/jpa23052023/Jpa23052023Application.java
+++ src/main/java/com/softtek/jpa23052023/Jpa23052023Application.java
@@ -1,6 +1,7 @@
 package com.softtek.jpa23052023;
 
 import com.softtek.jpa23052023.modelo.Cliente;
+import com.softtek.jpa23052023.modelo.Orden;
 import com.softtek.jpa23052023.modelo.Producto;
 import com.softtek.jpa23052023.servicio.ClienteServicio;
 import com.softtek.jpa23052023.servicio.OrdenServicio;
@@ -11,6 +12,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.util.Comparator;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @SpringBootApplication
 public class Jpa23052023Application implements CommandLineRunner {
@@ -27,7 +32,9 @@ public class Jpa23052023Application implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         System.out.println("SPRING Boot Application Running");
-    cs.obtenerClientes().forEach(System.out::println);
+        ps.obtenerProductos().forEach(System.out::println);
+        os.obtenerOrdenes().forEach(System.out::println);
+        cs.obtenerClientes().forEach(System.out::println);
 
         //Obtener todos los registros y todos los campos de la tabla de productos
         System.out.println("\n Obtener todos los registros y todos los campos de la tabla de productos");
@@ -109,24 +116,24 @@ public class Jpa23052023Application implements CommandLineRunner {
         System.out.println("\n Obtener de la tabla de Customers las columnas CustomerId, CompanyName, " +
                 "Pais, Obtener los clientes cuyo pais comience con la letra U,S,A");
         cs.obtenerClientes().stream().filter(cliente -> cliente.getCountry().equalsIgnoreCase("USA"))
-             .map(cliente -> cliente.getCustomerId() + " " + cliente.getCompanyName() + " " + cliente.getCountry())
-             .forEach(System.out::println);
+                .map(cliente -> cliente.getCustomerId() + " " + cliente.getCompanyName() + " " + cliente.getCountry())
+                .forEach(System.out::println);
 
         //Obtener de la tabla de Productos las columnas productid, ProductName, UnitPrice cuyos precios esten entre 50 y 150
         System.out.println("\n Obtener de la tabla de Productos las columnas productid, ProductName, " +
                 "UnitPrice cuyos precios esten entre 50 y 150");
         ps.obtenerProductos().stream().filter(producto ->
                         producto.getPrecio() >= 50 && producto.getPrecio() <= 150)
-              .map(producto -> producto.getId() + " " + producto.getNombre() + " " + producto.getPrecio())
-              .forEach(System.out::println);
+                .map(producto -> producto.getId() + " " + producto.getNombre() + " " + producto.getPrecio())
+                .forEach(System.out::println);
 
         //Obtener de la tabla de Productos las columnas productid, ProductName, UnitPrice, UnitsInStock
         // cuyas existencias esten entre 50 y 100
         System.out.println("\n Obtener de la tabla de Productos las columnas productid, ProductName, " +
                 "UnitPrice, UnitsInStock cuyas existencias esten entre 50 y 100");
         ps.obtenerProductos().stream().filter(producto ->
-                producto.getUnidadesDisponibles() >= 50 && producto.getUnidadesDisponibles() <= 100)
-                .map(producto ->producto.getId() + " " + producto.getNombre() + " " + producto.getPrecio()
+                        producto.getUnidadesDisponibles() >= 50 && producto.getUnidadesDisponibles() <= 100)
+                .map(producto -> producto.getId() + " " + producto.getNombre() + " " + producto.getPrecio()
                         + " " + producto.getUnidadesDisponibles())
                 .forEach(System.out::println);
         //Obtener las columnas OrderId, CustomerId, employeeid de la tabla de ordenes cuyos empleados sean 1, 4, 9
@@ -134,14 +141,14 @@ public class Jpa23052023Application implements CommandLineRunner {
                 "cuyos empleados sean 1, 4, 9");
         os.obtenerOrdenes().stream().filter(orden -> orden.getIdEmpleado() == 1 ||
                         orden.getIdEmpleado() == 4 || orden.getIdEmpleado() == 9)
-              .map(orden -> orden.getId() + " " + orden.getIdCliente() + " " + orden.getIdEmpleado())
-              .forEach(System.out::println);
+                .map(orden -> orden.getId() + " " + orden.getIdCliente() + " " + orden.getIdEmpleado())
+                .forEach(System.out::println);
 
         //Obtener la información de la tabla de Products, Ordenarlos por Nombre del Producto de forma ascendente
         System.out.println("\n Obtener la información de la tabla de Products, Ordenarlos por Nombre del Producto " +
                 "de forma ascendente");
         ps.obtenerProductos().stream().sorted(Comparator.comparing(Producto::getNombre))
-               .forEach(System.out::println);
+                .forEach(System.out::println);
 
         //Obtener la información de la tabla de Products, Ordenarlos por Categoria de forma ascendente
         // y por precio unitario de forma descendente
@@ -149,7 +156,7 @@ public class Jpa23052023Application implements CommandLineRunner {
                 "y por precio unitario de forma descendente");
         ps.obtenerProductos().stream()
                 .sorted(Comparator.comparing(Producto::getCategoria)
-                .thenComparing(Comparator.comparing(Producto::getPrecio).reversed()))
+                        .thenComparing(Comparator.comparing(Producto::getPrecio).reversed()))
                 .forEach(System.out::println);
 
         //Obtener la información de la tabla de Clientes, Customerid, CompanyName, city, country ordenar por pais,
@@ -157,8 +164,8 @@ public class Jpa23052023Application implements CommandLineRunner {
         System.out.println("\n Obtener la información de la tabla de Clientes, Customerid, CompanyName, " +
                 "city, country ordenar por pais, ciudad de forma ascendente");
         cs.obtenerClientes().stream().sorted(Comparator.comparing(Cliente::getCity)
-              .thenComparing(Comparator.comparing(Cliente::getCountry)))
-              .forEach(System.out::println);
+                        .thenComparing(Comparator.comparing(Cliente::getCountry)))
+                .forEach(System.out::println);
 
         //Obtener los productos productid, productname, categoryid, supplierid
         // ordenar por categoryid y supplier únicamente mostrar aquellos cuyo precio esté entre 25 y 200
@@ -195,14 +202,123 @@ public class Jpa23052023Application implements CommandLineRunner {
         System.out.println("\nObtener los datos de los clientes(Customers) ordenados descendentemente " +
                 "por nombre(CompanyName) que se encuentren en la ciudad(city) de barcelona, Lisboa");
         cs.obtenerClientes().stream()
-               .filter(cliente -> cliente.getCity().equalsIgnoreCase("Barcelona") ||
-                       cliente.getCity().equalsIgnoreCase("Lisboa"))
-               .sorted(Comparator.comparing(Cliente::getCompanyName).reversed())
-               .forEach(System.out::println);
+                .filter(cliente -> cliente.getCity().equalsIgnoreCase("Barcelona") ||
+                        cliente.getCity().equalsIgnoreCase("Lisboa"))
+                .sorted(Comparator.comparing(Cliente::getCompanyName).reversed())
+                .forEach(System.out::println);
+
+        //Obtener los datos de las ordenes, ordenados descendentemente por la fecha de la orden cuyo cliente(CustomerId) sea ALFKI
+        System.out.println("\nObtener los datos de las ordenes, ordenados descendentemente " +
+                "por la fecha de la orden cuyo cliente(CustomerId) sea ALFKI");
+        os.obtenerOrdenes().stream()
+                .filter(orden -> Objects.equals(orden.getIdCliente(), "ALFKI"))
+                .sorted(Comparator.comparing(Orden::getFecha).reversed())
+                .forEach(System.out::println);
+
+        //Obtener los datos de las ordenes ordenados ascendentemente por la fecha de la orden cuyo empleado sea 2 o 4
+        System.out.println("\nObtener los datos de las ordenes ordenados ascendentemente " +
+                "por la fecha de la orden cuyo empleado sea 2 o 4");
+        os.obtenerOrdenes().stream()
+                .filter(orden -> orden.getIdEmpleado() == 2 || orden.getIdEmpleado() == 4)
+                .sorted(Comparator.comparing(Orden::getFecha))
+                .forEach(System.out::println);
+
+        //Obtener los productos cuyo precio están entre 30 y 60 ordenado por nombre
+        System.out.println("\nObtener los productos cuyo precio están entre 30 y 60 ordenado por nombre");
+        ps.obtenerProductos().stream()
+                .filter(producto -> producto.getPrecio() >= 30 && producto.getPrecio() <= 60)
+                .sorted(Comparator.comparing(Producto::getNombre))
+                .forEach(System.out::println);
+
+        //Numero de productos por categoria
+        System.out.println("\nNumero de productos por categoria");
+        ps.obtenerProductos().stream()
+                .collect(Collectors.groupingBy(Producto::getCategoria, Collectors.counting()))
+                .forEach((categoria, count) ->
+                        System.out.println(categoria + " " + count));
+
+        //Obtener el precio promedio por proveedor de la tabla de productos
+        System.out.println("\nObtener el precio promedio por proveedor de la tabla de productos");
+        ps.obtenerProductos().stream()
+                .collect(Collectors.groupingBy(Producto::getProveedor, Collectors.averagingDouble(Producto::getPrecio)))
+                .forEach((proveedor, precio) ->
+                        System.out.println(proveedor + " " + precio));
+
+        //Obtener la suma de inventario (UnitsInStock) por SupplierID De la tabla de productos (Products)
+        System.out.println("\nObtener la suma de inventario (UnitsInStock) por SupplierID De la tabla de productos (Products)");
+        ps.obtenerProductos().stream()
+                .collect(Collectors.groupingBy(Producto::getProveedor, Collectors.summingDouble(Producto::getUnidadesDisponibles)))
+                .forEach((proveedor, suma) ->
+                        System.out.println(proveedor + " " + suma));
+
+        //Contar las ordenes por cliente de la tabla de orders
+        System.out.println("\nContar las ordenes por cliente de la tabla de orders");
+        os.obtenerOrdenes().stream()
+                .collect(Collectors.groupingBy(Orden::getIdCliente, Collectors.counting()))
+                .forEach((cliente, count) ->
+                        System.out.println(cliente + " " + count));
+
+        //Contar las ordenes por empleado de la tabla de ordenes unicamente del empleado 1,3,5,6
+        System.out.println("\nContar las ordenes por empleado de la tabla de ordenes unicamente del empleado 1,3,5,6");
+        os.obtenerOrdenes().stream()
+                .filter(orden -> orden.getIdEmpleado() == 1 || orden.getIdEmpleado() == 3 || orden.getIdEmpleado() == 5 || orden.getIdEmpleado() == 6)
+               .collect(Collectors.groupingBy(Orden::getIdEmpleado, Collectors.counting()))
+               .forEach((empleado, count) ->
+                        System.out.println(empleado + " " + count));
+
+        //Obtener la suma del envío (freight) por cliente
+        System.out.println("\nObtener la suma del envío (freight) por cliente");
+        os.obtenerOrdenes().stream()
+                .filter(orden -> orden.getTransporte() != null)
+               .collect(Collectors.groupingBy(Orden::getIdCliente, Collectors.summingDouble(Orden::getTransporte)))
+               .forEach((cliente, suma) ->
+                        System.out.println(cliente + " " + suma));
+
+        //De la tabla de ordenes únicamente de los registros cuya ShipCity sea Madrid, Sevilla,
+        // Barcelona, Lisboa, London Ordenado por el campo de suma del envio
+        System.out.println("\nDe la tabla de ordenes únicamente de los registros cuya ShipCity sea Madrid, " +
+                "Sevilla, Barcelona, Lisboa, London Ordenado por el campo de suma del envio");
+        os.obtenerOrdenes().stream()
+              .filter(orden -> orden.getShipCity() != null && (orden.getShipCity().equals("Madrid") || orden.getShipCity().equals("Sevilla") ||
+                      orden.getShipCity().equals("Barcelona") || orden.getShipCity().equals("Lisboa") ||
+                      orden.getShipCity().equals("London")))
+              .collect(Collectors.groupingBy(Orden::getShipCity, Collectors.summingDouble(orden ->
+                      Optional.ofNullable(orden.getTransporte()).orElse(0.0))))
+                .entrySet().stream()
+                .sorted(Map.Entry.comparingByValue())
+                .forEach(entry -> System.out.println(entry.getKey() + " " + entry.getValue()));
 
 
+        //obtener el precio promedio de los productos por categoria sin contar con los productos descontinuados (Discontinued)
+        System.out.println("\nObtener el precio promedio de los productos por categoria sin contar con los productos descontinuados (Discontinued)");
+        ps.obtenerProductos().stream()
+               .collect(Collectors.groupingBy(Producto::getCategoria, Collectors.averagingDouble(Producto::getPrecio)))
+               .forEach((categoria, precio) ->
+                        System.out.println(categoria + " " + precio));
+
+        //Obtener la cantidad de productos por categoria,  aquellos cuyo precio se encuentre entre 10 y 60 que
+        // tengan más de 12 productos
+        System.out.println("\nObtener la cantidad de productos por categoria,  aquellos cuyo precio se encuentre entre 10 y 60 que " +
+                "tengan más de 12 productos");
+        ps.obtenerProductos().stream()
+              .filter(producto -> producto.getPrecio() >= 10 && producto.getPrecio() <= 60)
+              .collect(Collectors.groupingBy(Producto::getCategoria, Collectors.counting()))
+                .entrySet().stream()
+                .filter(entry -> entry.getValue() > 9)
+                .forEach(entry -> System.out.println("Categoría: " + entry.getKey() + ", Cantidad: " + entry.getValue()));
+
+        //OBTENER LA SUMA DE LAS UNIDADES EN EXISTENCIA (UnitsInStock) POR CATEGORIA,
+        // Y TOMANDO EN CUENTA UNICAMENTE LOS PRODUCTOS CUYO PROVEEDOR (SupplierID) SEA IGUAL A 17, 19, 16.
+        //--cuya categoria tenga menos de 100 unidades ordenado por unidades
+        System.out.println("\nOBTENER LA SUMA DE LAS UNIDADES EN EXISTENCIA (UnitsInStock) POR CATEGORIA, " +
+                "Y TOMANDO EN CUENTA UNICAMENTE LOS PRODUCTOS CUYO PROVEEDOR (SupplierID) SEA IGUAL A 17, 19, 16.");
+        ps.obtenerProductos().stream()
+                .filter(producto -> producto.getProveedor() == 17 || producto.getProveedor() == 19 || producto.getProveedor() == 16)
+                .collect(Collectors.groupingBy(Producto::getCategoria, Collectors.summingInt(Producto::getUnidadesDisponibles)))
+                .entrySet().stream()
+                .filter(entry -> entry.getValue() < 200)
+                .sorted(Comparator.comparingInt(Map.Entry::getValue))
+                .forEach(entry -> System.out.println("Categoría: " + entry.getKey() + ", Suma de unidades: " + entry.getValue()));
     }
-
-
-    }
+}
 
